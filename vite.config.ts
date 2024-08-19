67955b91c2b065f9607a8a1314a425a951d173d3
MJ Cheriyan <midhuncheriyan@gmail.com>
2024-08-19 23:04:31 +0100

    Added Gh Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import viteReact from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 export default defineConfig({
+  base: '/pokedex/',
   plugins: [TanStackRouterVite(), viteReact()],
   resolve: {
     alias: {
